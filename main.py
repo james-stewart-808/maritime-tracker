8488a919ea070494f9a71d8f5acded1af5d919f9
james <61799526+james-stewart-808@users.noreply.github.com>
2026-03-30 10:44:04 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,24 +7,26 @@ st.header("Welcome to the Homepage of the International Shipping Dashboard 🏡"
 #country_iso_codes = pd.read_csv("/country_iso_codes.csv")#, usecols=country_iso_codes_cols, dtype=country_iso_codes_dtype).rename(columns=country_iso_codes_renames)
 #country_iso_codes.loc[country_iso_codes.iso_country == "Namibia", "iso_2"] = "NA"
 
+#https://raw.githubusercontent.com/<your_Github_username>/<your_repository_name>/<branch_name>/<file_name>.<extension_name>
+
 def load_original_data():
-    url = 'https://raw.githubusercontent.com/james-stewart-808/inventory-tracker/blob/1ecaad7e067e4d9016790fabe59a2c5691cfe764/datasets/dom_inv_by_vess_type.csv'
+    url = 'https://raw.githubusercontent.com/james-stewart-808/inventory-tracker/main/datasets/dom_inv_by_vess_type.csv'
     response = requests.get(url)
     if response.status_code == 200:
         return pd.read_csv(StringIO(response.text), nrows=2)
     else:
         st.error("Failed to load data from GitHub.")
         return None
-#country_iso_codes = load_original_data()
+country_iso_codes = load_original_data()
 
 #country_iso_codes = pd.read_csv("https://raw.githubusercontent.comjames-stewart-808/inventory-tracker/blob/1ecaad7e067e4d9016790fabe59a2c5691cfe764/datasets/dom_inv_by_vess_type.csv")
 
 # Function to load the CSV file
-@st.cache_data
+#@st.cache_data
 def load_data(file):
     data = pd.read_csv(file)
     return data
 
-country_iso_codes = load_data('https://raw.githubusercontent.comjames-stewart-808/inventory-tracker/blob/1ecaad7e067e4d9016790fabe59a2c5691cfe764/datasets/dom_inv_by_vess_type.csv')
+#country_iso_codes = load_data('https://raw.githubusercontent.comjames-stewart-808/inventory-tracker/blob/1ecaad7e067e4d9016790fabe59a2c5691cfe764/datasets/dom_inv_by_vess_type.csv')
 st.write(country_iso_codes)
 #
