af7951d86023d18af6848bfbe45c601ae89f0d3f
james <61799526+james-stewart-808@users.noreply.github.com>
2026-04-08 11:11:10 +0100

    Fix syntax error in impact_tracking.py

--- impact_tracking.py
+++ impact_tracking.py
@@ -124,6 +124,7 @@ else:
             y=alt.Y(
                 alt.repeat("layer"),
                 aggregate="mean",
-                title="Mean of Compliance Costs (US$bn)")),
+                title="Mean of Compliance Costs (US$bn)"),
+            color=alt.datum(alt.repeat("layer")),
             ).repeat(layer=["EU ETS (US$bn)", "IMO NZF (US$bn)"])
     
