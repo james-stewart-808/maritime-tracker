d1b54edf4bc95d3d6c7a8c2c7c0a01b0d9b78456
james <61799526+james-stewart-808@users.noreply.github.com>
2026-04-21 10:03:02 +0100

    Including logo in sidebar

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,26 @@
 import streamlit as st
 import pandas as pd
 
+# logo at the top of sidebar
+st.markdown(
+    """
+    <style>
+    [data-testid="stSidebarNav"]::before {
+        content: "";
+        display: block;
+        margin: 0px auto 20px auto;
+        height: 120px;
+        width: 180px;
+        background-image: url(https://raw.githubusercontent.com/yosephis/maritime-tracker/main/UCL_Logo_S_1C_B_RGB_Energy-Inst.png);
+        background-size: contain;
+        background-repeat: no-repeat;
+        background-position: center;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
 # Define the pages
 main = st.Page("main.py", title="Home", icon="🏡")
 inventories = st.Page("inventories.py", title="Voyage-based Inventories", icon="🚢")
