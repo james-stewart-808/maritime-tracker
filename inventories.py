a44aa25a635275aef360a7c5ca5275b9ba454c93
james <61799526+james-stewart-808@users.noreply.github.com>
2026-04-24 14:45:21 +0100

    Update inventories.py

--- inventories.py
+++ inventories.py
@@ -31,10 +31,10 @@ N_ene_co2 = st.segmented_control(
 if N_ene_co2 == None:
     N_ene_co2 = "Number of Calls"
 
+
 ### INVENTORIES BY VESSEL TYPE ###
 st.header("{0} by Vessel Type".format(
     N_ene_co2))
-
 
 # Read-in International Arrivals Inventory by Vessel Type Associated with the Country
 int_arr_by_type_to_plot_cols = ["Int. Arr. by Type", "n_vys", "ene_tj", "co2e_t"]
@@ -109,6 +109,90 @@ download_as_csv(
     "International Arrivals Inventory by Vessel Type - {0} ({1}).csv".format(
         st.session_state.iso_country, st.session_state.iso_code)
 )
+
+
+
+
+### INVENTORIES BY PARTNER ECONOMY ###
+st.header("{0} by Partner Economy".format(
+    N_ene_co2))
+
+# Read-in International Arrivals Inventory by Vessel Type Associated with the Country
+int_arr_by_partner_to_plot_cols = ["Int. Arr. by Partner", "n_vys", "ene_tj", "co2e_t"]
+int_arr_by_partner_to_plot = pd.read_csv(\
+    input_dir + "inventories_v0.2/{0}/int_arr_by_partner.csv".format(
+        st.session_state.iso_code.replace(' ','%20')
+    ), usecols=int_arr_by_partner_to_plot_cols
+)
+
+int_arr_by_partner_to_plot["inv_type"] = "Int. Arrivals"
+int_arr_by_partner_to_plot_col_renames = {
+    "Int. Arr. by Partner": "Partner Economy", "n_vys": "Number of Calls", 
+    "ene_tj": "Energy Demand (TJ)", "co2e_t": "GHG Emissions (t CO2e)"
+}
+int_arr_by_partner_to_plot = int_arr_by_partner_to_plot.rename(
+    columns=int_arr_by_partner_to_plot_col_renames
+)
+
+
+# Read-in International Departures Inventory by Vessel Type Associated with the Country
+int_dep_by_partner_to_plot_cols = ["Int. Dep. by Partner", "n_vys", "ene_tj", "co2e_t"]
+int_dep_by_partner_to_plot = pd.read_csv(
+    input_dir + "inventories_v0.2/{0}/int_dep_by_partner.csv".format(
+        st.session_state.iso_code.replace(' ','%20')
+    ), usecols=int_dep_by_partner_to_plot_cols
+)
+
+int_dep_by_partner_to_plot["inv_type"] = "Int. Departures"
+int_dep_by_partner_to_plot_col_renames = {
+    "Int. Dep. by Partner": "Partner Economy", "n_vys":"Number of Calls", 
+    "ene_tj":"Energy Demand (TJ)", "co2e_t":"GHG Emissions (t CO2e)"
+}
+int_dep_by_partner_to_plot = int_dep_by_partner_to_plot.rename(
+    columns=int_dep_by_partner_to_plot_col_renames
+)
+
+
+# Combine Int. Arrivals and Int. Departures Inventories for Plotting
+int_inv_by_partner_to_plot = pd.concat([int_arr_by_partner_to_plot, int_dep_by_partner_to_plot], axis=0)
+
+# Plot depending on the value of Segmented Control
+if N_ene_co2 == "Number of Calls":
+    st.altair_chart(
+        alt.Chart(
+            int_inv_by_partner_to_plot).mark_bar().encode(
+            x=alt.X("Partner Economy", sort='-y'),
+            y="Number of Calls", # alt.Y(y, sort='-x')
+            color="Number of Calls")
+    )
+
+elif N_ene_co2 == "Energy Demand":
+    st.bar_chart(
+        int_inv_by_partner_to_plot, 
+        x="Partner Economy", 
+        y="Energy Demand (TJ)", 
+        color="inv_type", 
+        stack=False
+    )
+else:
+    st.bar_chart(
+        int_inv_by_partner_to_plot, 
+        x="Partner Economy", 
+        y="GHG Emissions (t CO2e)", 
+        color="inv_type", 
+        stack=False
+    )
+
+# Provide option to download as CSV
+download_as_csv(
+    int_inv_by_partner_to_plot, 
+    "International Arrivals Inventory by Partner Economy - {0} ({1})".format(
+        st.session_state.iso_country, st.session_state.iso_code),
+    "International Arrivals Inventory by Partner Economy - {0} ({1}).csv".format(
+        st.session_state.iso_country, st.session_state.iso_code)
+)
+
+
 
 
 ### INVENTORIES BY PORT ###
