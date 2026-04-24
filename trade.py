9546e9bc69a8bde45d6ac24e07aeec4cac645557
james <61799526+james-stewart-808@users.noreply.github.com>
2026-04-24 14:11:16 +0100

    Update trade.py

--- trade.py
+++ trade.py
@@ -24,7 +24,9 @@ def download_as_csv(file, label, filename):
 
 st.title("Merchandise Trade Portfolio for {0}".format(
     st.session_state.iso_country))
-st.sidebar.markdown("International Merchandise Trade Portfolios Sourced from UNCTAD's Trade-and-Transport Database.")
+st.sidebar.markdown(
+    "International Merchandise Trade Portfolios Sourced from UNCTAD's Trade-and-Transport Database."
+)
 
 # Consider Imports or Exports?
 I_X = st.segmented_control(
@@ -190,30 +192,54 @@ else:
     )
 
 
-st.header("A note on Quality Assurance")
-st.markdown(" - It's important to note that UNCTAD's Trade-and-Transport Database is fundamentally based on Comtrade data.")
-st.markdown(" - The quality of data hosted on Comtrade is highly variable depending on the country and region.")
-st.markdown(" - Presented below is the Comtrade contribution record for the selected country of interest over the decade from 2014-23.")
-st.markdown(" - Please bear this information in-mind when exploring the trade data - merchandise trade statistics will be more reliable for countries that have provided their data often over recent years.")
+st.header(
+    "A note on Quality Assurance"
+)
+st.markdown(
+    " - It's important to note that UNCTAD's Trade-and-Transport Database is fundamentally \
+    based on Comtrade data."
+)
+st.markdown(
+    " - The quality of data hosted on Comtrade is highly variable depending on the country and region."
+)
+st.markdown(
+    " - Presented below is the Comtrade contribution record for the selected country of interest over \
+    the decade from 2014-23."
+)
+st.markdown(
+    " - Please bear this information in-mind when exploring the trade data - merchandise trade statistics \
+    will be more reliable for countries that have provided their data often over recent years."
+)
 
 by_country_contrib_record_v1 = pd.read_csv(
     input_dir + "by_country_contrib_record_v1.csv")
 #st.write(by_country_contrib_record_v1)
 
 def com_cont_aggregator(df, aggregator):
-    df_group_cols = {"iso_code":"count", "2014":"sum", "2015":"sum", "2016":"sum", "2017":"sum", "2018":"sum", "2019":"sum", "2020":"sum", "2021":"sum", "2022":"sum", "2023":"sum"}
+    df_group_cols = {
+        "iso_code":"count", 
+        "2014":"sum", "2015":"sum", "2016":"sum", "2017":"sum", "2018":"sum", 
+        "2019":"sum", "2020":"sum", "2021":"sum", "2022":"sum", "2023":"sum"
+    }
     df_grouped = df.groupby(aggregator).agg(df_group_cols).reset_index()
-    df_grouped["Contributions"] = df_grouped["2014"] + df_grouped["2015"] + df_grouped["2016"] + df_grouped["2017"] + df_grouped["2018"] + df_grouped["2019"] + df_grouped["2020"] + df_grouped["2021"] + df_grouped["2022"] + df_grouped["2023"]
+    df_grouped["Contributions"] = df_grouped["2014"] + df_grouped["2015"] + df_grouped["2016"] + \
+        df_grouped["2017"] + df_grouped["2018"] + df_grouped["2019"] + df_grouped["2020"] + \
+        df_grouped["2021"] + df_grouped["2022"] + df_grouped["2023"]
     df_grouped["Agg Totals"] = df_grouped["iso_code"] * 10
     df_grouped["Contribution (pct)"] = np.round(100 * df_grouped["Contributions"] / df_grouped["Agg Totals"], 1)
     return df_grouped#[[aggregator, "Contribution (pct)"]]
 
 
 st.subheader("Overall Contribution Record of All States")
-contributed = sum([by_country_contrib_record_v1[x].value_counts().values[0] for x in ["2014", "2015", "2016", "2017", "2018", "2019", "2020", "2021", "2022", "2023"]])
+contributed = sum([by_country_contrib_record_v1[x].value_counts().values[0] for x in \
+                   ["2014", "2015", "2016", "2017", "2018", "2019", "2020", "2021", "2022", "2023"]])
 total = by_country_contrib_record_v1.shape[0] * 10
 contributed_frac = np.round(100 * contributed / (by_country_contrib_record_v1.shape[0] * 10), 1)
-st.write("All countries for which data is available: {0} contributions over the last 10 years out of a possible {1}, representing an overall record of {2}%".format(contributed, total, contributed_frac))
+st.write(
+    "All countries for which data is available: {0} contributions over the last 10 years out of a possible {1}, \
+    representing an overall record of {2}%".format(
+        contributed, total, contributed_frac)
+)
 
 comtrade_rec_cou = by_country_contrib_record_v1[(by_country_contrib_record_v1.iso_code == float(st.session_state.iso_code))]
 if comtrade_rec_cou.shape[0] == 0:
@@ -233,7 +259,12 @@ else:
     c9.metric("2022", comtrade_rec_cou["2022"].values[0])
     c10.metric("2023", comtrade_rec_cou["2023"].values[0])
 
-    st.write("The contribution record of {0} over the last 10 years is shown above, representing an average contribution rate of {1}%. Further information on Comtrade Contribution Rates is provided below.".format(st.session_state.iso_country, int(100 * comtrade_rec_cou.iloc[:, -10:].sum().sum() / 10)))
+    st.write(
+        "The contribution record of {0} over the last 10 years is shown above, representing an average contribution rate \
+        of {1}%. Further information on Comtrade Contribution Rates is provided below.".format(
+            st.session_state.iso_country, int(100 * comtrade_rec_cou.iloc[:, -10:].sum().sum() / 10)
+        )
+    )
 
 
     st.subheader("Contribution of Statistics by Region")
