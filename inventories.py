9916fbf38517106269ee6b5e3c367fbab01740d8
james <61799526+james-stewart-808@users.noreply.github.com>
2026-04-24 14:25:43 +0100

    Update inventories.py

--- inventories.py
+++ inventories.py
@@ -11,6 +11,7 @@ def download_as_csv(file, label, filename):
             label=label,
             file_name=filename)
 
+
 ##### INTERNATIONAL VOYAGE-BASED ACTIVITY INVENTORIES ######
 # Re-write subset output scripts in terms country codes (e.g. '007') and include 'Disaggregation by Partner Countries'.
 
@@ -34,40 +35,45 @@ if N_ene_co2 == None:
 st.header("{0} by Vessel Type".format(
     N_ene_co2))
 
+
 # Read-in International Arrivals Inventory by Vessel Type Associated with the Country
-int_arr_by_type_to_plot_cols = ["int_arr_type", "n_vys", "ene_tj", "co2_t"]
+int_arr_by_type_to_plot_cols = ["Int. Arr. by Type", "n_vys", "ene_tj", "co2e_t"]
 int_arr_by_type_to_plot = pd.read_csv(\
-    input_dir + "inventories/{0}/int_arr_inv_by_vess_type.csv".format(
-        st.session_state.iso_country.replace(' ','%20')
-    ), usecols=int_arr_by_type_to_plot_cols)
+    input_dir + "inventories_v0.2/{0}/int_arr_by_type.csv".format(
+        st.session_state.iso_code.replace(' ','%20')
+    ), usecols=int_arr_by_type_to_plot_cols
+)
 
 int_arr_by_type_to_plot["inv_type"] = "Int. Arrivals"
 int_arr_by_type_to_plot_col_renames = {
     "int_arr_type":"Vessel Type", "n_vys":"Number of Calls", 
-    "ene_tj":"Energy Demand (TJ)", "co2_t":"Carbon Dioxide (t)"
+    "ene_tj":"Energy Demand (TJ)", "co2e_t":"GHG Emissions (t CO2e)"
 }
 int_arr_by_type_to_plot = int_arr_by_type_to_plot.rename(
     columns=int_arr_by_type_to_plot_col_renames
 )
 
+
 # Read-in International Departures Inventory by Vessel Type Associated with the Country
-int_dep_by_type_to_plot_cols = ["int_dep_type", "n_vys", "ene_tj", "co2_t"]
+int_dep_by_type_to_plot_cols = ["Int. Dep. by Type", "n_vys", "ene_tj", "co2e_t"]
 int_dep_by_type_to_plot = pd.read_csv(
-    input_dir + "inventories/{0}/int_dep_inv_by_vess_type.csv".format(
-        st.session_state.iso_country.replace(' ','%20')
-    ), usecols=int_dep_by_type_to_plot_cols)
+    input_dir + "inventories_v0.2/{0}/int_dep_by_type.csv".format(
+        st.session_state.iso_code.replace(' ','%20')
+    ), usecols=int_dep_by_type_to_plot_cols
+)
+
 int_dep_by_type_to_plot["inv_type"] = "Int. Departures"
 int_dep_by_type_to_plot_col_renames = {
     "int_dep_type":"Vessel Type", "n_vys":"Number of Calls", 
-    "ene_tj":"Energy Demand (TJ)", "co2_t":"Carbon Dioxide (t)"
+    "ene_tj":"Energy Demand (TJ)", "co2e_t":"GHG Emissions (t CO2e)"
 }
 int_dep_by_type_to_plot = int_dep_by_type_to_plot.rename(
     columns=int_dep_by_type_to_plot_col_renames
 )
 
+
 # Combine Int. Arrivals and Int. Departures Inventories for Plotting
 int_inv_by_type_to_plot = pd.concat([int_arr_by_type_to_plot, int_dep_by_type_to_plot], axis=0)
-
 
 # Plot depending on the value of Segmented Control
 if N_ene_co2 == "Number of Calls":
@@ -75,7 +81,8 @@ if N_ene_co2 == "Number of Calls":
         alt.Chart(int_arr_by_type_to_plot).mark_bar().encode(
             x=alt.X("Vessel Type", sort='-y'),
             y="Number of Calls", # alt.Y(y, sort='-x')
-            color="Number of Calls"))
+            color="Number of Calls")
+    )
 
 elif N_ene_co2 == "Energy Demand":
     st.bar_chart(
@@ -89,7 +96,7 @@ else:
     st.bar_chart(
         int_inv_by_type_to_plot, 
         x="Vessel Type", 
-        y="Carbon Dioxide (t)", 
+        y="GHG Emissions (t CO2e)", 
         color="inv_type", 
         stack=False
     )
@@ -108,30 +115,31 @@ download_as_csv(
 st.header("{0} by Port of Interest".format(N_ene_co2))
 
 # Read-in International Arrivals Inventory by Vessel Type Associated with the Country
-int_arr_by_port_to_plot_cols = ["int_arr_port", "n_vys", "ene_tj", "co2_t"]
+int_arr_by_port_to_plot_cols = ["int_arr_port", "n_vys", "ene_tj", "co2e_t"]
 int_arr_by_port_to_plot = pd.read_csv(
-    input_dir + "inventories/{0}/int_arr_inv_by_port.csv".format(
-        st.session_state.iso_country.replace(' ','%20')
+    input_dir + "inventories_v0.2/{0}/int_arr_inv_by_port.csv".format(
+        st.session_state.iso_code.replace(' ','%20')
     ), usecols=int_arr_by_port_to_plot_cols)
 int_arr_by_port_to_plot["inv_type"] = "Int. Arrivals"
 int_arr_by_port_to_plot_col_renames = {
     "int_arr_port":"Port", "n_vys":"Number of Calls", 
-    "ene_tj":"Energy Demand (TJ)", "co2_t":"Carbon Dioxide (t)"
+    "ene_tj":"Energy Demand (TJ)", "co2e_t":"Carbon Dioxide (t)"
 }
 int_arr_by_port_to_plot = int_arr_by_port_to_plot.rename(
     columns=int_arr_by_port_to_plot_col_renames
 )
 
 # Read-in International Departures Inventory by Vessel Type Associated with the Country
-int_dep_by_port_to_plot_cols = ["int_dep_port", "n_vys", "ene_tj", "co2_t"] #
+int_dep_by_port_to_plot_cols = ["int_dep_port", "n_vys", "ene_tj", "co2e_t"]
 int_dep_by_port_to_plot = pd.read_csv(
-    input_dir + "inventories/{0}/int_dep_inv_by_port.csv".format(
-        st.session_state.iso_country.replace(' ','%20')
+    input_dir + "inventories_v0.2/{0}/int_dep_inv_by_port.csv".format(
+        st.session_state.iso_code.replace(' ','%20')
     ), usecols=int_dep_by_port_to_plot_cols)
+
 int_dep_by_port_to_plot["inv_type"] = "Int. Departures"
 int_dep_by_port_to_plot_col_renames = {
     "int_dep_port":"Port", "n_vys":"Number of Calls", 
-    "ene_tj":"Energy Demand (TJ)", "co2_t":"Carbon Dioxide (t)"
+    "ene_tj":"Energy Demand (TJ)", "co2e_t":"GHG Emissions (t CO2e)"
 }
 int_dep_by_port_to_plot = int_dep_by_port_to_plot.rename(
     columns=int_dep_by_port_to_plot_col_renames
@@ -160,7 +168,7 @@ else:
     st.bar_chart(
         int_inv_by_port_to_plot, 
         x="Port", 
-        y="Carbon Dioxide (t)", 
+        y="GHG Emissions (t CO2e)", 
         color="inv_type", 
         stack=False
     )
