0d39714cee543aec2936438a8a7a28bb0f7017fa
james <61799526+james-stewart-808@users.noreply.github.com>
2026-05-02 16:09:59 +0100

    Update inventories.py

--- inventories.py
+++ inventories.py
@@ -81,7 +81,7 @@ int_dep_by_type["inv_type"] = "Int. Departures"
 # Combine Int. Arrivals and Int. Departures Inventories for Plotting
 int_inv_by_type_to_plot = pd.concat([int_arr_by_type, int_dep_by_type], axis=0)
 st.write(int_inv_by_type_to_plot)
-st.write(int_inv_by_type_to_plot.dtype)
+st.write(int_inv_by_type_to_plot.dtypes)
 
 # Plot depending on the value of Segmented Control
 if indicator in ["Number of Calls"]:
