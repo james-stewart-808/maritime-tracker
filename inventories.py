6e6940d5e38047d6976f2e7cccb4e8ab422471de
james <61799526+james-stewart-808@users.noreply.github.com>
2026-05-02 15:24:02 +0100

    Add 'NZF Costs in 2050 (US$)' to inventories

--- inventories.py
+++ inventories.py
@@ -26,7 +26,7 @@ st.sidebar.markdown(
 
 indicator = st.segmented_control(
     "Which indicator would you like to visualise?",
-    ["Number of Calls", "Energy Demand (TJ)", "GHG Emissions (t CO2e)", "NZF Costs"]
+    ["Number of Calls", "Energy Demand (TJ)", "GHG Emissions (t CO2e)", "NZF Costs in 2050 (US$)"]
 )
 if indicator == None:
     indicator = "Number of Calls"
@@ -37,7 +37,7 @@ st.header("{0} by Vessel Type".format(
     indicator))
 
 # Read-in International Arrivals Inventory by Vessel Type Associated with the Country
-int_arr_by_type_to_plot_cols = ["Int. Arr. by Type", "n_vys", "ene_tj", "co2e_t"]
+int_arr_by_type_to_plot_cols = ["Int. Arr. by Type", "n_vys", "ene_tj", "co2e_t", "s24_50"]
 int_arr_by_type_to_plot = pd.read_csv(\
     input_dir + "inventories_v0.2/{0}/int_arr_by_type.csv".format(
         st.session_state.iso_code.replace(' ','%20')
@@ -47,7 +47,8 @@ int_arr_by_type_to_plot = pd.read_csv(\
 int_arr_by_type_to_plot["inv_type"] = "Int. Arrivals"
 int_arr_by_type_to_plot_col_renames = {
     "Int. Arr. by Type": "Vessel Type", "n_vys":"Number of Calls", 
-    "ene_tj":"Energy Demand (TJ)", "co2e_t":"GHG Emissions (t CO2e)"
+    "ene_tj":"Energy Demand (TJ)", "co2e_t":"GHG Emissions (t CO2e)",
+    "s24_50":"NZF Costs in 2050 (US$)"
 }
 int_arr_by_type_to_plot = int_arr_by_type_to_plot.rename(
     columns=int_arr_by_type_to_plot_col_renames
@@ -55,7 +56,7 @@ int_arr_by_type_to_plot = int_arr_by_type_to_plot.rename(
 
 
 # Read-in International Departures Inventory by Vessel Type Associated with the Country
-int_dep_by_type_to_plot_cols = ["Int. Dep. by Type", "n_vys", "ene_tj", "co2e_t"]
+int_dep_by_type_to_plot_cols = ["Int. Dep. by Type", "n_vys", "ene_tj", "co2e_t", "s24_50"]
 int_dep_by_type_to_plot = pd.read_csv(
     input_dir + "inventories_v0.2/{0}/int_dep_by_type.csv".format(
         st.session_state.iso_code.replace(' ','%20')
@@ -65,7 +66,8 @@ int_dep_by_type_to_plot = pd.read_csv(
 int_dep_by_type_to_plot["inv_type"] = "Int. Departures"
 int_dep_by_type_to_plot_col_renames = {
     "Int. Dep. by Type": "Vessel Type", "n_vys":"Number of Calls", 
-    "ene_tj":"Energy Demand (TJ)", "co2e_t":"GHG Emissions (t CO2e)"
+    "ene_tj":"Energy Demand (TJ)", "co2e_t":"GHG Emissions (t CO2e)",
+    "s24_50":"NZF Costs in 2050 (US$)"
 }
 int_dep_by_type_to_plot = int_dep_by_type_to_plot.rename(
     columns=int_dep_by_type_to_plot_col_renames
@@ -110,7 +112,7 @@ st.header("{0} by Partner Economy".format(
     indicator))
 
 # Read-in International Arrivals Inventory by Vessel Type Associated with the Country
-int_arr_by_partner_to_plot_cols = ["Int. Arr. by Partner", "n_vys", "ene_tj", "co2e_t"]
+int_arr_by_partner_to_plot_cols = ["Int. Arr. by Partner", "n_vys", "ene_tj", "co2e_t", "s24_50"]
 int_arr_by_partner_to_plot = pd.read_csv(\
     input_dir + "inventories_v0.2/{0}/int_arr_by_partner.csv".format(
         st.session_state.iso_code.replace(' ','%20')
@@ -120,7 +122,8 @@ int_arr_by_partner_to_plot = pd.read_csv(\
 int_arr_by_partner_to_plot["inv_type"] = "Int. Arrivals"
 int_arr_by_partner_to_plot_col_renames = {
     "Int. Arr. by Partner": "Partner Economy", "n_vys": "Number of Calls", 
-    "ene_tj": "Energy Demand (TJ)", "co2e_t": "GHG Emissions (t CO2e)"
+    "ene_tj": "Energy Demand (TJ)", "co2e_t": "GHG Emissions (t CO2e)",
+    "s24_50":"NZF Costs in 2050 (US$)"
 }
 int_arr_by_partner_to_plot = int_arr_by_partner_to_plot.rename(
     columns=int_arr_by_partner_to_plot_col_renames
@@ -128,7 +131,7 @@ int_arr_by_partner_to_plot = int_arr_by_partner_to_plot.rename(
 
 
 # Read-in International Departures Inventory by Vessel Type Associated with the Country
-int_dep_by_partner_to_plot_cols = ["Int. Dep. by Partner", "n_vys", "ene_tj", "co2e_t"]
+int_dep_by_partner_to_plot_cols = ["Int. Dep. by Partner", "n_vys", "ene_tj", "co2e_t", "s24_50"]
 int_dep_by_partner_to_plot = pd.read_csv(
     input_dir + "inventories_v0.2/{0}/int_dep_by_partner.csv".format(
         st.session_state.iso_code.replace(' ','%20')
@@ -138,7 +141,8 @@ int_dep_by_partner_to_plot = pd.read_csv(
 int_dep_by_partner_to_plot["inv_type"] = "Int. Departures"
 int_dep_by_partner_to_plot_col_renames = {
     "Int. Dep. by Partner": "Partner Economy", "n_vys":"Number of Calls", 
-    "ene_tj":"Energy Demand (TJ)", "co2e_t":"GHG Emissions (t CO2e)"
+    "ene_tj":"Energy Demand (TJ)", "co2e_t":"GHG Emissions (t CO2e)",
+    "s24_50":"NZF Costs in 2050 (US$)"
 }
 int_dep_by_partner_to_plot = int_dep_by_partner_to_plot.rename(
     columns=int_dep_by_partner_to_plot_col_renames
@@ -183,7 +187,7 @@ download_as_csv(
 st.header("{0} by Port".format(indicator))
 
 # Read-in International Arrivals Inventory by Vessel Type Associated with the Country
-int_arr_by_port_to_plot_cols = ["Int. Arr. by Port", "n_vys", "ene_tj", "co2e_t"]
+int_arr_by_port_to_plot_cols = ["Int. Arr. by Port", "n_vys", "ene_tj", "co2e_t", "s24_50"]
 int_arr_by_port_to_plot = pd.read_csv(
     input_dir + "inventories_v0.2/{0}/int_arr_by_port.csv".format(
         st.session_state.iso_code.replace(' ','%20')
@@ -191,14 +195,15 @@ int_arr_by_port_to_plot = pd.read_csv(
 int_arr_by_port_to_plot["inv_type"] = "Int. Arrivals"
 int_arr_by_port_to_plot_col_renames = {
     "Int. Arr. by Port":"Port", "n_vys":"Number of Calls", 
-    "ene_tj":"Energy Demand (TJ)", "co2e_t":"GHG Emissions (t CO2e)"
+    "ene_tj":"Energy Demand (TJ)", "co2e_t":"GHG Emissions (t CO2e)",
+    "s24_50":"NZF Costs in 2050 (US$)"
 }
 int_arr_by_port_to_plot = int_arr_by_port_to_plot.rename(
     columns=int_arr_by_port_to_plot_col_renames
 )
 
 # Read-in International Departures Inventory by Vessel Type Associated with the Country
-int_dep_by_port_to_plot_cols = ["Int. Dep. by Port", "n_vys", "ene_tj", "co2e_t"]
+int_dep_by_port_to_plot_cols = ["Int. Dep. by Port", "n_vys", "ene_tj", "co2e_t", "s24_50"]
 int_dep_by_port_to_plot = pd.read_csv(
     input_dir + "inventories_v0.2/{0}/int_dep_by_port.csv".format(
         st.session_state.iso_code.replace(' ','%20')
@@ -207,7 +212,8 @@ int_dep_by_port_to_plot = pd.read_csv(
 int_dep_by_port_to_plot["inv_type"] = "Int. Departures"
 int_dep_by_port_to_plot_col_renames = {
     "Int. Dep. by Port":"Port", "n_vys":"Number of Calls", 
-    "ene_tj":"Energy Demand (TJ)", "co2e_t":"GHG Emissions (t CO2e)"
+    "ene_tj":"Energy Demand (TJ)", "co2e_t":"GHG Emissions (t CO2e)",
+    "s24_50":"NZF Costs in 2050 (US$)"
 }
 int_dep_by_port_to_plot = int_dep_by_port_to_plot.rename(
     columns=int_dep_by_port_to_plot_col_renames
