bb048ba8910251bb32c4ac1c4a92fc46b92f060c
james <61799526+james-stewart-808@users.noreply.github.com>
2026-05-19 10:15:51 +0100

    Add files via upload

---
+++ contact.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import requests
+from io import StringIO
+import streamlit as st
+st.header("Contact")
+
+st.write(
+"""
+UCL Shipping and Oceans Research Group
+
+UCL Energy Institute
+
+Central House
+
+14 Upper Woburn Place
+
+London WC1H 0NN
+
+shippingandoceans@ucl.ac.uk
+"""
+)
