3cbba6e566ac7374428b837af633cc1b12710046
james <61799526+james-stewart-808@users.noreply.github.com>
2026-05-22 12:35:00 +0100

    Update references.py

--- references.py
+++ references.py
@@ -9,17 +9,22 @@ st.write(
 CE Delft. (2021). Study on assessment of possible global regulatory measures to \
     reduce greenhouse gas emissions from international shipping. European \
     Commission.
+    
 DNV. (2024a). Report of the Comprehensive impact assessment of the basket of \
     candidate GHG reduction mid-term measures – full report on Task 2 (Impacts \
     on the fleet). MEPC 82-INF.8-Add.1.
+    
 Faber et al. (2020). Fourth IMO Greenhouse Gas Study. London.
+
 Hoffmeister and Dalheimer. (2024). A linear model to estimate modal split in \
     international freight transport, based on revealed preferences about cost \
     and time saving. J. Shipp. Trd. 9, 28. Available at: \
     https://doi.org/10.1186/s41072-024-00181-0.
+    
 IPCC (2006). 2006 IPCC Guidelines for National Greenhouse Gas Inventories. \
     Volume 2 (Energy), Chapter 3 on Mobile combustion – Section 5 on Water-borne \
     Navigation. Intergovernmental Panel on Climate Change.
+    
 UNCTAD. (2025). Trade-and-Transport Dataset - Documentation.
 """
 )
