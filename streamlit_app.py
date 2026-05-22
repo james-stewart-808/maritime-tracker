e404e09eb416dac1263bf90c588c27e03cd5f20e
james <61799526+james-stewart-808@users.noreply.github.com>
2026-05-22 12:53:13 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ st.markdown(
     """
     <style>
     [data-testid="stSidebarNav"]::before {
-        content: "Maritime Tracker Dashboard";
+        content: "";
         display: block;
         margin: 0px auto 20px auto;
         height: 120px;
